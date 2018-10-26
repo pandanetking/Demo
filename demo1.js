76543b932d035ee566549cd33a16283e6a7c44d8
Supakit kamonworarat <pandanetbarnes@hotmail.com>
2018-10-26 14:14:45 +0700

    ส่งครั้งที่4 แก้ไขไฟล์

--- demo1.js
+++ demo1.js
@@ -1,3 +1,6 @@
-console.log("MR.Supakit 6039010023");
-console.log("MR.Supakit 6039010023");
-console.log("MR.Supakit 6039010023");
+console.log("MR.Supakit ");
+console.log("Kamonworarat");
+console.log("6039010023");
+console.log("M2/2");
+console.log("CHONBURI");
+console.log("TECHNICALCOLLEGE")
